b68d9105d81d89fade30c03d741eba811d453b4d
Devaansh-Kumar <devaanshk840@gmail.com>
2023-09-07 17:08:30 +0530

    scanner

---
+++ test6.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#define num 10
+
+/*
+// comments
+*/
+
+/*
+/*
+*/
+*/
+
+
+int main() {
+        int i = num;
+        while(i >= 0) {
+                printf("%d ", i);
+                i--;
+        }
+        string ch = "calvin";
+        printf("%d", strlen(ch));
+        return 0;
+}
